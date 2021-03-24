cfa65ba704099cc1689fb2373f191965f8d4400e
ethan <preevete@gmail.com>
2021-03-23 21:00:01 -0600

    added several functions

--- js/v2/boardFunctions.js
+++ js/v2/boardFunctions.js
@@ -7,15 +7,42 @@ function selectQueenFromConflictsList () {
 }
 
 function generateColumnConflictsList(boardList, column) {
-  let slopeTree = new binaryNode();
-  for (let i=0;i<boardList.length;++i) {
-    countConflicts(board, slopeTree, queenLocation);
-
-
+  let slopeTree = createSlopeTree(boardList, column);
+  let conflictsList = [];
+  for (let row=0;row<boardList.length;++row) {
+    queenLocatrowon = new point(column, row);
+    conflictList[i] = countConflicts(board, slopeTree, queenLocation);
+  }
 }
 
 function countConflicts(board, slopeTree, queenLocation) {
 
 }
 
-function createSlopeTree(boardList, 
+function createSlopeTree(boardList, currentWorkingColumn) {
+  let tree = new binaryNode();
+}
+
+
+function generateAllCombinations(boardList) {
+  let combinationList = [];
+  for (let row=0;row<boardList.length;++row) {
+    for (let column=row+1;column<boardList.length;++column) {
+      pointA = new point(i, boardList[i]);
+      pointB = new point(j, boardList[j]);
+      combinationList.push([pointA, pointB]);
+    }
+  }
+  return combinationList;
+}
+
+function removeWorkingColumnFromCobinationList(combinationList, column) {
+  return combinationList.filter((item) => { 
+    let [pointA, pointB] = item;
+    if (pointA.x === column || pointB.x === column) {
+      return false;
+    }
+    return true;
+  });
+}
+
