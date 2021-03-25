beaabdc4ad4fb5a44d9cf830d57907ec18961e5d
ethan <preevete@gmail.com>
2021-03-24 18:21:57 -0600

    added countconflicts functions

--- js/v2/src/boardFunctions.js
+++ js/v2/src/boardFunctions.js
@@ -16,13 +16,50 @@ function generateColumnConflictsList(boardList, column) {
   return conflictsList;
 }
 
-function countConflicts(boardList, slopeTree, queenLocation) {
-  
 
+function countConflicts(boardList, slopeTree, queenLocation) {
+  let combinationList = generateSinglePointCombinations(boardList, queenLocation);
+  let count = 0;
+  for (let item=0;item<CombinationList.length;++item) {
+    [pointA, pointB] = CombinationList[item];
+    slopeIntercept = new slopeInterceptObject(
+        computeSlope(pointA, pointB),
+        computeYIntercept(pointA, pointB));
+    if ( slopeIntercept.slope === 1 || slopeIntercept.slope === -1 ) {
+      ++count;
+    } else if ( slopeIntercept.slope === 0 ) {
+      ++count;
+    } else {
+      if (slopeTree.find(slopeIntercept)) {
+        ++count;
+      }
+    }
+  }
+  return count;
 }
 
 function countConflictsToMax(boardList, slopeTree, queenLocation, max) {
-
+  let combinationList = generateSinglePointCombinations(boardList, queenLocation);
+  let count = 0;
+  for (let item=0;item<CombinationList.length;++item) {
+    [pointA, pointB] = CombinationList[item];
+    slopeIntercept = new slopeInterceptObject(
+        computeSlope(pointA, pointB),
+        computeYIntercept(pointA, pointB));
+    if ( slopeIntercept.slope === 1 || slopeIntercept.slope === -1 ) {
+      ++count;
+    } else if ( slopeIntercept.slope === 0 ) {
+      ++count;
+    } else {
+      if (slopeTree.find(slopeIntercept)) {
+        ++count;
+      }
+    }
+    if (count > max) {
+      break;
+    }
+  }
+  return count;
 }
 
 function createSlopeTree(boardList, currentWorkingColumn) {
@@ -45,15 +82,28 @@ function createSlopeTree(boardList, currentWorkingColumn) {
 
 function generateAllCombinations(boardList) {
   let combinationList = [];
-  for (let row=0;row<boardList.length;++row) {
-    for (let column=row+1;column<boardList.length;++column) {
-      pointA = new point(i, boardList[i]);
-      pointB = new point(j, boardList[j]);
+  for (let column=0;column<boardList.length;++column) {
+    for (let row=column+1;row<boardList.length;++row) {
+      pointA = new point(column, boardList[i]);
+      pointB = new point(row, boardList[j]);
       combinationList.push([pointA, pointB]);
     }
   }
   return combinationList;
 }
+
+function generateSinglePointCombinations(boardList, point) {
+  let combinationList = [];
+  let listPoint = new point(0, 0);
+  for (let i=0;i<boardList.length;++i) {
+    listPoint = new point(i, boardList[i]);
+    if (point.x != listPoint.x) {
+      combinationList.push([point, listPoint]);
+    }
+  }
+  return combinationList
+}
+
 
 function removeWorkingColumnFromCombinationList(combinationList, column) {
   return combinationList.filter((item) => { 
