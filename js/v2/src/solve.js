be1ab0d8d90eeb4f4bd3123a1c8912e494d06ad1
ethan <preevete@gmail.com>
2021-03-24 19:19:42 -0600

    finished output and boilerplate for solve

--- js/v2/src/solve.js
+++ js/v2/src/solve.js
@@ -8,6 +8,8 @@ let maxTries = 1000;
 let maxRounds = 1;
 let triesNeededForSolution = 0;
 
+let boardlist = [];
+
 for (let rounds=0;rounds<maxRounds;++rounds) {
   for (let tries=0;tries<maxTries;++tries) {
     boardList = generate.newBoardGreedy(size);
@@ -15,4 +17,16 @@ for (let rounds=0;rounds<maxRounds;++rounds) {
 
 
 
-if (
+  }
+}
+
+boardErrors = checks.isValidBoard(boardList);
+if (boardErrors === 0){
+  output.printBoard(boardList);
+  console.log("actual boardList", boardList);
+  output.printSolution(boardList);
+} else {
+  console.log("not a valid solution, number of errors:", boardErrors);
+}
+console.log("tries needed average", triesNeeded/maxRounds);
+
